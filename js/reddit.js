192c169b2174559c57f557c9e864c45609812094
EC2 Default User <ec2-user@ip-172-31-45-64.us-east-2.compute.internal>
2018-05-09 00:11:36 +0000

    trying to get this to work

--- js/reddit.js
+++ js/reddit.js
@@ -9,23 +9,29 @@ xhr.send();
 // onload is "new" and is just readyState === 4
 xhr.onload = function() {
   var risingJSON = xhr.response;
-  var statusHTML = '<ul>';
   var time = new Date().getTime();
+
+
+  var statusHTML = '<table id="#reddittable"><tr> <th>score/min</th> <th>score</th> <th>min</th> <th>headline</th> </tr>' ;
+
 
   for(var i=0 ; i < risingJSON['data']['children'].length; i++) {
       var title = risingJSON['data']['children'][i]['data'].title ;
       var score = risingJSON['data']['children'][i]['data'].score ;
 
+      
       // reddit stores utc in seconds but getTime is in milliseconds
       var age = Math.round(((time/1000) - risingJSON['data']['children'][i]['data'].created_utc) / 60) ;
 
       //round to nearest .1 (10*hot/10=hot)
       var hot = Math.round(10*(score / age)) / 10;
 
-      // add list element
-      statusHTML += `<li>${hot} (${score}score / ${age}m) ${title}</li>`;
+      // add row
+      statusHTML += `<tr> <td>${hot}</td> <td>${score}</td> <td>${age}</td> <td>${title}</td> </tr>`;
   }
 
-  statusHTML += '</ul>';
+  statusHTML += '</table>';
   document.getElementById('rising').innerHTML = statusHTML;
-};
+  document.getElementById('#reddittable').DataTable();
+
+}; 
