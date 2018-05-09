099b84bc6cae82ea6e9e99778c5989df49bbeb7e
EC2 Default User <ec2-user@ip-172-31-45-64.us-east-2.compute.internal>
2018-05-09 00:42:29 +0000

    basic style working with datatable plugin

--- js/reddit.js
+++ js/reddit.js
@@ -6,32 +6,31 @@ xhr.open('GET', 'https://www.reddit.com/r/politics.json?limit=1000');
 xhr.responseType = 'json';
 xhr.send();
 
-// onload is "new" and is just readyState === 4
 xhr.onload = function() {
   var risingJSON = xhr.response;
   var time = new Date().getTime();
 
 
-  var statusHTML = '<table id="#reddittable"><tr> <th>score/min</th> <th>score</th> <th>min</th> <th>headline</th> </tr>' ;
+  var dataArray =[];
 
 
   for(var i=0 ; i < risingJSON['data']['children'].length; i++) {
       var title = risingJSON['data']['children'][i]['data'].title ;
       var score = risingJSON['data']['children'][i]['data'].score ;
 
-      
       // reddit stores utc in seconds but getTime is in milliseconds
       var age = Math.round(((time/1000) - risingJSON['data']['children'][i]['data'].created_utc) / 60) ;
 
-      //round to nearest .1 (10*hot/10=hot)
+      //round to nearest .1
       var hot = Math.round(10*(score / age)) / 10;
 
-      // add row
-      statusHTML += `<tr> <td>${hot}</td> <td>${score}</td> <td>${age}</td> <td>${title}</td> </tr>`;
+      // add data to array
+      dataArray.push([hot, score, age, title]);
   }
+  
+  console.log(dataArray);
 
-  statusHTML += '</table>';
-  document.getElementById('rising').innerHTML = statusHTML;
-  document.getElementById('#reddittable').DataTable();
-
+  //load data into #example table...
+  $('#example').DataTable( { data: dataArray } );
+  
 }; 
